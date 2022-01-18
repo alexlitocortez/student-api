ff6c3cfc20f0b89da05eb5c5757a6716a38b13ef
Alex Cortez <acim650@gmail.com>
2022-01-18 08:55:45 -0800

    finally accessed array

--- src/components/API/SearchBar.js
+++ src/components/API/SearchBar.js
@@ -1,34 +1,54 @@
-import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 function SearchBar() {
     const [APIData, setAPIData] = useState([])
+    const [searchInput, setSearchInput] = useState('');
+    const [filteredResults, setFilteredResults] = useState([]);
+
     useEffect(() => {
         axios.get('https://api.hatchways.io/assessment/students')
             .then((response) => {
-                setAPIData(response.data);
-            }
-            catch(error) {
-                console.log(error.message);
-            }
+                setAPIData(response.data.students);
+                console.log(response.data.students);
+            })
+            .catch(err => {
+                console.log(err)
+            })
     }, [])
 
+    const searchItems = (searchValue) => {
+        setSearchInput(searchValue)
+        if (searchInput !== '') {
+        const filteredData = APIData.filter((item) => {
+            return Object.values(item).join('').toLowerCase().includes(searchInput.toLowerCase())
+        })
+        setFilteredResults(filteredData)
+        } else {
+            setFilteredResults(APIData)
+        }
+    }
 
     return (
         <div>
-            <input className='search-bar' placeholder='Search by name...' />
-            {/* {students.map((student) => {
-                return (
-                    <p>{student}</p>
-                )
-            })} */}
+            <input className='search-bar' placeholder='Search by name...' onChange={(e) => searchItems(e.target.value)} />
+                <div>
+                    {searchInput.length > 1 ? (
+                        filteredResults.map((item) => {
+                            return (
+                                <div>
+                                    <div>{item.firstName + item.lastName}</div>
+                                </div>
+                            )
+                        })
+                    )}
+                </div>
         </div>
     )
 }
 
+
+
 export default SearchBar
-
-
-
 
 
